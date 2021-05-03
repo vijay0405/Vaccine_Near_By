e3b847b25f79a30a00fb4637a3e075dd8700ff77
vijay0405 <uvijaysimha@gmail.com>
2021-05-03 22:41:46 +0530

    added standard api requests to get districts & centers

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
-
 const app = express();
+const schedule = require('node-schedule');
+const axios = require('axios');
 
-app.get("/", (req, res)=>{
+axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/32')
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
+
+  axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=581&date=04-05-2021')
+  .then(res => {
+    console.log(JSON.stringify(res.data))
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
+
+
+
+app.get("/getData", (req, res)=>{
     res.send("Hi you are not alone !!!");
 })
 
