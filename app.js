b14c899dbb5ddfdcb4fecf0b3d06039f7e3ce5de
vijay0405 <uvijaysimha@gmail.com>
2021-05-03 22:56:23 +0530

    fetch and store states

--- app.js
+++ app.js
@@ -2,30 +2,48 @@ const express = require('express');
 const app = express();
 const schedule = require('node-schedule');
 const axios = require('axios');
-
-axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/32')
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => {
-    console.log('Error: ', err.message);
-  });
-
-  axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=581&date=04-05-2021')
-  .then(res => {
-    console.log(JSON.stringify(res.data))
-  })
-  .catch(err => {
-    console.log('Error: ', err.message);
-  });
+const fs = require('fs')
 
 
+axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
+    .then(res => {
+        console.log(res.data.states)
+        fs.writeFile('states.json', JSON.stringify(res.data.states), err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            //file written successfully
+        })
+    })
+    .catch(err => {
+        console.log('Error: ', err.message);
+    });
 
-app.get("/getData", (req, res)=>{
+
+// axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/32')
+//   .then(res => {
+//     console.log(res.data['districts'])
+//   })
+//   .catch(err => {
+//     console.log('Error: ', err.message);
+//   });
+
+//   axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=581&date=04-05-2021')
+//   .then(res => {
+//     console.log(JSON.stringify(res.data))
+//   })
+//   .catch(err => {
+//     console.log('Error: ', err.message);
+//   });
+
+
+
+app.get("/getData", (req, res) => {
     res.send("Hi you are not alone !!!");
 })
 
 
-app.listen(3061, ()=>{
+app.listen(3061, () => {
     console.log("listening on port 3061")
 })
